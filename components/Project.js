0a7b4b97c1ebe7ca3a2e260df32e906f8e8dcf25
Ryan Dinul Fatah <ryandfbasketball12@gmail.com>
2022-06-20 22:10:51 +0700

    😇 Optimize image and fix bugs again

--- components/Project.js
+++ components/Project.js
@@ -7,22 +7,20 @@ export default function Project() {
   const [item, setItem] = useState(userData.project)
   return (
     <>
-      <React.Fragment>
-        <div className="">
-          <div id="work">
-            <h1 className="text-center text-4xl font-extrabold text-slate-800 dark:text-white lg:ml-1">
-              Here you can see my work&#128640;
-            </h1>
-            {/* New Card */}
-            <div className="xs:grid-cols-1 grid items-center justify-center gap-4 py-2 px-2 md:grid-cols-2">
-              {item.map((project) => (
-                <CardProject key={project.id} project={project} />
-              ))}
-            </div>
-            {/* End */}
+      <div className="">
+        <div id="work">
+          <h1 className="text-center text-4xl font-extrabold text-slate-800 dark:text-white lg:ml-1">
+            Here you can see my work&#128640;
+          </h1>
+          {/* New Card */}
+          <div className="xs:grid-cols-1 grid items-center justify-center gap-4 py-2 px-2 md:grid-cols-2">
+            {item.map((project) => (
+              <CardProject key={project.id} project={project} />
+            ))}
           </div>
+          {/* End */}
         </div>
-      </React.Fragment>
+      </div>
     </>
   )
 }
